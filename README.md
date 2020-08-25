53a963a2ebef5383d37742cb522ecfe10df59652
emrecanalgul <can.emre1995@gmail.com>
2020-08-25 16:02:59 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Niwi Kimlik Sorgulama 
+
+
+T.C. İçişleri Bakanlığı Nüfus ve Vatandaşlık İşleri Genel Müdürlüğü tarafından kullanıma sunulan web servisler üzerinden kimlik doğrulama yapılmasını sağlamaktadır.
+
+[![NuGet version (Niwi)](https://img.shields.io/nuget/v/Niwi.svg?style=flat-square)](https://www.nuget.org/packages/Niwi/)
+
+
+## Kurulum
+```bash
+Install-Package Niwi
+# ve ya
+dotnet add package Niwi
+```
+## Kullanım
+
+```csharp
+using NiwiLib;
+
+Niwi.TcKimlikNoDogrula(52321242857,"Ad","Soyad",1995);
+Niwi.YabanciKimlikNoDogrula(52321242857 ,"Ad","Soyad" 12, 1, 1995);
+
+```
---
+++ src/NiwiLib/Niwi.cs
@@ -0,0 +1,30 @@
+﻿using KPSPublicYabanciDogrula;
+using System;
+using System.Threading.Tasks;
+
+namespace NiwiLib
+{
+    public static class Niwi
+    {
+        public static bool TcKimlikNoDogrula(long tcKimlik, string ad, string soyad, int dogumYili)
+        {
+            KPSPublic.KPSPublicSoapClient client = new KPSPublic.KPSPublicSoapClient(KPSPublic.KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            var response = client.TCKimlikNoDogrulaAsync(tcKimlik, ad, soyad, dogumYili).Result;
+            return response.Body.TCKimlikNoDogrulaResult;
+        }
+
+        public static bool KisiVeCuzdanDogrula(long tCKimlikNo, string ad, string soyad, bool soyadYok, int? dogumGun, bool dogumGunYok, int? dogumAy, bool dogumAyYok, int dogumYil, string cuzdanSeri, int? cuzdanNo, string tCKKSeriNo)
+        {
+            KPSPublicV2.KPSPublicV2SoapClient client = new KPSPublicV2.KPSPublicV2SoapClient(KPSPublicV2.KPSPublicV2SoapClient.EndpointConfiguration.KPSPublicV2Soap);
+            var response = client.KisiVeCuzdanDogrulaAsync(tCKimlikNo, ad, soyad, soyadYok, dogumGun, dogumGunYok, dogumAy, dogumAyYok, dogumYil, cuzdanSeri, cuzdanNo, tCKKSeriNo).Result;
+            return response.Body.KisiVeCuzdanDogrulaResult;
+        }
+
+        public static bool YabanciKimlikNoDogrula(long kimlikNo, string ad, string soyad, int? dogumGun, int? dogumAy, int dogumYil)
+        {
+            KPSPublicYabanciDogrulaSoapClient client = new KPSPublicYabanciDogrulaSoapClient(KPSPublicYabanciDogrulaSoapClient.EndpointConfiguration.KPSPublicYabanciDogrulaSoap);
+            var response = client.YabanciKimlikNoDogrulaAsync(kimlikNo, ad, soyad, dogumGun, dogumAy, dogumYil).Result;
+            return response.Body.YabanciKimlikNoDogrulaResult;
+        }
+    }
+}
